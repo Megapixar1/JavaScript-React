801ce9770c5f665890361010bd7e241401e42bff
Dima <example@gmail.com>
2022-10-07 14:30:50 +0300

    обєкти і деструктирізація

--- js/script.js
+++ js/script.js
@@ -677,16 +677,50 @@
 
 //console.log(fib());
 
-'Callback - function'; 
-function learnJS(leng, callback) {
+//'Callback - function'; 
+//function learnJS(leng, callback) {
 
-    console.log(`Я учу: ${leng}`);
-    callback();
+//    console.log(`Я учу: ${leng}`);
+//    callback();
 
-}
+//}
 
-function done() {
-    console.log('Я прошол этот урок');
-}
+//function done() {
+//    console.log('Я прошол этот урок');
+//}
 
-learnJS('JavaScript', done);
+//learnJS('JavaScript', done);
+
+
+//const options = {
+//    name: 'test',
+//    width: 1024,
+//    height: 1024,
+//    colors: {
+//        border: 'black',
+//        bg: 'red'
+//    },
+//    makeTest: function() {
+//        console.log("Test");
+//    }
+//};
+//options.makeTest();
+
+//const {border, bg} = options.colors;
+//console.log(border); 
+//console.log(Object.keys(options).length);
+//console.log(options['colors']['border']);
+//let counter = 0;
+//for (let key in options) {
+//    if (typeof(options[key]) === 'object') {
+//        for (let i in options[key]) {
+//            console.log(`Свойстo: ${key} имеет значение ${options[key][i]}`);
+//            counter++;
+//        }
+//    } else {
+//         console.log(`Свойстo: ${key} имеет значение ${options[key]}`);
+//         counter++;
+//    }
+//}
+
+  
