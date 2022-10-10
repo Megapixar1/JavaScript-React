1a553b9f2a8110485cfb1c2c8284eb9a2919089a
Dima <example@gmail.com>
2022-10-10 16:06:32 +0300

    sread operator

--- js/script.js
+++ js/script.js
@@ -755,3 +755,94 @@
 //function compareNum(a, b) {
 //    return a - b;
 //}
+
+//let a = 5,
+//    b = a;
+
+//b += 5; 
+
+//console.log(b);
+//console.log(a);
+
+
+//const obj = {
+//    a: 5,
+//    b: 1
+//};
+
+//const copy = obj;
+//copy.a = 10;
+
+//console.log(obj);
+//console.log(copy);
+
+
+//function copy(mainObj) {
+//    let objCopy = {};
+
+//    let key;
+//    for (key in mainObj) {
+//        objCopy[key] = mainObj[key];
+//    }
+
+//    return objCopy;
+//}
+
+//const numbers = {
+//    a: 2,
+//    b: 5,
+//    c: {
+//        x: 7,
+//        y: 4
+//    }
+//};
+
+//const newNumers = copy(numbers);
+//newNumers.a = 10;
+//console.log(newNumers);
+
+
+//const add = {
+//    d: 17,
+//    e: 20
+//};
+//const clone = Object.assign({}, add);
+//clone.d = 20;
+//console.log(add);
+//console.log(clone);
+
+
+//const oldArray = ['a', 'b', 'c'];
+//const newArray = oldArray.slice();
+
+//newArray[1] = 'Kolosovskyi';
+
+//console.log(newArray);
+//console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...blogs, ...video, 'vk', 'facebook'];      
+
+console.log(internet);  
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+const num = [2, 5, 7];
+
+log(...num);
+
+
+const array = ['a', 'b'];
+
+const newAarraey = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q}
